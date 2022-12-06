31987965ed74d58c9b7c13acdedd292bf6ae71e8
FianFebryIspianto <hendryfebry01@gmail.com>
2022-12-06 20:26:02 +0700

    Login feature done

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  name: "ButtonComponent",
+  props: ["page"],
+};
+</script>
+<template>
+  <div class="d-grid">
+    <button type="submit" class="btn btn-lg btn-primary">{{ page }}</button>
+  </div>
+</template>
