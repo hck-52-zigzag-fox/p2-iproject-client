d25e9d8382c55671063c7fa1109b23d044fcc629
Nadya <nadya.rere@gmail.com>
2022-12-06 21:06:13 +0700

    setup client

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
-    },
+    
   ],
 });
+
+// router.beforeEach((to, from, next) => {
+//   const isAuthenticated = localStorage.getItem("access_token");
+//   if (isAuthenticated && (to.name === "login" || to.name === "register")) {
+//     next({ name: "products" });
+//   } else if (!isAuthenticated && to.name === "favorites") {
+//     Swal.fire("Please login first")
+//     next({ name: "login" });
+//   } else next();
+// });
 
 export default router;
---
+++ src/stores/index.js
@@ -0,0 +1,15 @@
+import { defineStore } from "pinia";
+import axios from "axios";
+import Swal from "sweetalert2";
+
+// const baseUrl = "https://zigzag-fox-nadya.up.railway.app/";
+// const baseUrl = "http://localhost:3000/";
+
+export const useStore = defineStore("store", {
+  state: () => ({
+
+  }),
+  actions: {
+    
+  },
+});
