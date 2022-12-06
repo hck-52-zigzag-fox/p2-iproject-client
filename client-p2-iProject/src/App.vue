bf231d8b65c53f2f5614039828cf2106855f5dbf
Markus <markus.sarsanto@gmail.com>
2022-12-06 17:05:47 +0700

    Skeleton client

---
+++ client-p2-iProject/src/App.vue
@@ -0,0 +1,9 @@
+<script>
+import { RouterView, RouterLink } from "vue-router";
+export default {
+  name: "App",
+};
+</script>
+<template>
+  <RouterView/>
+</template>
---
+++ client-p2-iProject/src/views/LoginPage.vue
@@ -0,0 +1,8 @@
+<script>
+export default {
+    name : "LoginPage"
+}
+</script>
+<template>
+
+</template>
