1c5b98a6b5ee5854fcf461c77d361b041fee9098
FianFebryIspianto <hendryfebry01@gmail.com>
2022-12-07 10:24:54 +0700

    Edit profile donee

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import LoginView from "../views/LoginView.vue";
+import ProfileEditView from "../views/ProfileEditView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       path: "/login",
       name: "Login",
       component: LoginView,
+    },
+    {
+      path: "/profile/edit",
+      name: "Edit Profile",
+      component: ProfileEditView,
     },
   ],
 });
