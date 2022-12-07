bfb98b41d4587e8e88f09145f7eae60eb26b6ccd
Sony MelliFriady <zoenasun@gmail.com>
2022-12-07 09:18:07 +0700

    [create] init-vue-project-done

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import { RouterView } from "vue-router";
+
+export default {
+  name: "App",
+  components: {
+    RouterView,
+  },
+};
+</script>
+
+<template>
+  <RouterView />
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+  name: "HomeVue",
+};
+</script>
+
+<template>
+  <div>Lorem, ipsum.</div>
+</template>
