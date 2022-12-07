401c3685bdc71776956f51eacef224ccfe43cb0a
unknown <ian_buzz@hotmail.com>
2022-12-07 13:22:37 +0700

    done

---
+++ SpaceEdu/src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<script>
+
+
+export default {
+    name:"Navbar"
+}
+</script>
+
+<template>
+    <div class="container" style="margin-top: 25px;">
+    <header class="d-flex justify-content-center py-3 bg-secondary rounded-pill">
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="#" class="nav-link"> <RouterLink class="nav-link text-dark" to="/">Home</RouterLink></a></li>
+       
+        <li class="nav-item"><a href="#" class="nav-link"><RouterLink to="/solarsystem" class="nav-link text-dark">Solar System</RouterLink></a></li>
+        <li class="nav-item"><a href="#" class="nav-link"><RouterLink to="/about" class="nav-link text-dark">About</RouterLink></a></li>
+      </ul>
+    </header>
+  </div>
+</template>
