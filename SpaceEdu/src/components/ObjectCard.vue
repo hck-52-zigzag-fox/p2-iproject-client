0a7fb0ec8e76f62b70fb737a3730ad69bcca989a
unknown <ian_buzz@hotmail.com>
2022-12-07 21:54:52 +0700

    done

--- SpaceEdu/src/components/ObjectCard.vue
+++ SpaceEdu/src/components/ObjectCard.vue
@@ -1,13 +1,23 @@
 <script>
+import { mapActions } from 'pinia';
+import { useCounterStore } from '../stores/counter';
+
 
 export default {
     name:"ObjectCard",
-    props:["object"]
+    props:["object"],
+    methods:{
+        ...mapActions(useCounterStore,['getObjectDetail']),
+        // getObjectDetailLocal() {
+        //     this.getObjectDetail()
+        // }
+
+    }
 }
 </script>
 
 <template>
-   <div class="col-md-4">
+   <div class="col-md-4 text-center">
       <div class="card" style="width: 18rem;">
   <img style="object-fit:contain;" height="200" width="auto" :src="object.imageUrl" class="card-img-top" alt="...">
   <div class="card-body">
@@ -16,7 +26,7 @@ export default {
         <p class=" mb-2 text-muted">Type: {{object.type}}</p>
 
     <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
-       <a href="#" class="btn mr-2"><i class="fas fa-link"></i> Visit Site</a>
+       <a href="#" class="btn mr-2 text-light " @click.prevent="getObjectDetail(object.name)"><i class="fas fa-link text-light" ></i> View Detail</a>
     <!-- <a href="#" class="btn "><i class="fab fa-github"></i> Github</a> -->
   </div>
   </div>
@@ -40,7 +50,7 @@ export default {
   border-image-slice: 1;
   background: var(--gradient) !important;
   -webkit-background-clip: text !important;
-  -webkit-text-fill-color: transparent !important;
+  /* -webkit-text-fill-color: transparent !important; */
   border-image-source:  var(--gradient) !important; 
   text-decoration: none;
   transition: all .4s ease;
---
+++ SpaceEdu/src/components/PictureCard.vue
@@ -0,0 +1,27 @@
+<script>
+
+
+export default {
+    name: "PictureCard",
+    props:["news"],
+    data(){
+        return {
+            date:this.news.data[0].date_created.substr(0,10)
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="card flex-md-row mb-4 box-shadow h-md-250">
+            <div class="card-body d-flex flex-column align-items-start">
+              <!-- <strong class="d-inline-block mb-2 text-primary">World</strong> -->
+              <h3 class="mb-0">
+                <a class="text-white" href="#">{{news.data[0].title}}</a>
+              </h3>
+              <div class="mb-1 text-muted">{{date}}</div>
+              <p class="card-text mb-auto">{{news.data[0].description}}</p>
+            </div>
+            <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" style="width: 200px; height: 250px;" :src="news.links[0].href" data-holder-rendered="true">
+          </div>
+</template>
