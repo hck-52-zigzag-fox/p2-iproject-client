d1759600d730c63878cfd866931639356f0e6a81
Nadya <nadya.rere@gmail.com>
2022-12-08 09:49:54 +0700

    deploy

--- src/stores/index.js
+++ src/stores/index.js
@@ -2,8 +2,8 @@ import { defineStore } from "pinia";
 import axios from "axios";
 import Swal from "sweetalert2";
 
-// const baseUrl = "https://zigzag-fox-nadya.up.railway.app/";
-const baseUrl = "http://localhost:3000";
+const baseUrl = "https://zigzag-fox-nadya.up.railway.app";
+// const baseUrl = "http://localhost:3000";
 
 export const useStore = defineStore("store", {
   state: () => ({
